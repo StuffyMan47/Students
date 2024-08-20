deefdcda5c3c37764ce8b8d680f0743c1fcfe7e0
StuffyMan47 <fazleev_01@mail.ru>
2024-08-20 19:51:25 +0300

    Add project files.

---
+++ Models/Estimate.cs
@@ -0,0 +1,8 @@
+﻿namespace Students.Models;
+
+public class Estimate
+{
+    public int Id { get; set; }
+    public int Value { get; set; }
+    public DateTime Date { get; set; }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace Students.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string ShortName { get; set; }
+}
